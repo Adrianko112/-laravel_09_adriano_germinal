9f116bb75283f7b8ae8292c51f6725b147f474e3
Adriano Germinal <adrianogerminal@gmail.com>
2026-04-10 19:44:39 +0200

    progetto 1

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Chi Siamo</title>
+    <!-- css custom -->
+     <link rel="stylesheet" href="/style.css">
+        <!-- css bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+     <!-- icone bootstrap -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+  </head>
+  <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-dark border-bottom" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/"><i class="bi bi-activity"></i></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/chi-siamo">Chi Siamo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/contatti">Contatti</a>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- navbar end -->
+   <!-- header -->
+    <header>
+        <div class="container-fluid header d-flex align-items-center justify-content-center">
+            <div class="row align-items-center h-100">
+               <div class="col-6">
+                <h2 class="text-white text-center text-color display-4 fw-bold">Chi siamo</h2>
+                <p class="text-white text-center text-color display-6">Siamo un gruppo di appassionati di fitness dedicati a aiutarti a raggiungere i tuoi obiettivi di allenamento.</p>
+               </div>
+               <div class="col-6">
+                <img src="/media/pt.jpg" alt="I nostri Personal Trainer" class="img-fluid rounded-3 shadow">
+               </div>
+            </div>
+        </div>
+    </header>
+   
+   
+   
+   
+   
+   
+   
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>GymBro</title>
+    <!-- css custom -->
+     <link rel="stylesheet" href="/style.css">
+        <!-- css bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+     <!-- icone bootstrap -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+  </head>
+  <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-dark border-bottom" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/"><i class="bi bi-activity"></i></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/chi-siamo">Chi Siamo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/contatti">Contatti</a>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- navbar end -->
+   <!-- header -->
+    <header>
+        <div class="container-fluid header d-flex align-items-center justify-content-center">
+            <div class="row align-items-center h-100">
+                <div class="col-12 d-flex flex-column align-items-center justify-content-center h-100">
+                    <h1 class="text-center display-3 mt-5 fw-bold text-color">Benvenuti su GymBro</h1>
+                    <p class="text-center display-6 text-color">Il tuo compagno di allenamento numero uno!</p>
+
+                </div>
+            </div>
+        </div>
+    </header>
+   
+   
+   
+   
+   
+   
+   
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
